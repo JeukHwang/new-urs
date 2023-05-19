2f9bbe6f4870ab95ac654cd3bc00db6642886fdb
CHOI <ball@CHOIs-MacBook-Air.local>
2023-05-20 07:09:42 +0900

    feature: add reserve api

--- src/router/reservation.router.js
+++ src/router/reservation.router.js
@@ -1,3 +1,4 @@
+import get_location_status_json from "../processor/getLocationStatus";
 export const reservationRouter = express.Router();
 
 // Make a reservation
@@ -11,3 +12,14 @@ reservationRouter.delete("/reservation", (req, res) => {
     // TODO: Implement this function
     return null;
 });
+
+//check reservation status
+reservationRouter.get("/reservation/location", async (req, res) => {
+    //TODO: Implement this function
+    const { locationId, searchDate } = req.body;
+
+    const reservation_status_JSON = await get_location_status_json(locationId, searchDate);
+
+    res.json(reservation_status_JSON);
+
+})
