1efd1a493558c9a0ae8586508ce5bdbe723037c0
CHOI <ball@CHOIs-MacBook-Air.local>
2023-05-20 07:09:13 +0900

    feature: add cancel reservation url

--- src/processor/url.js
+++ src/processor/url.js
@@ -4,3 +4,4 @@ export const resource_url = (resourceId) => `https://urs.kaist.ac.kr/urs/rsv/sch
 export const reserve_resource_url = "https://urs.kaist.ac.kr/urs/rsv/app/cmn/RsvAppCmn001M03.do";
 export const location_reservation_status_url = "https://urs.kaist.ac.kr/urs/rsv/app/cmn/RsvAppCmn001M11.do";
 export const resource_reservation_url = "https://urs.kaist.ac.kr/urs/rsv/app/cmn/RsvAppCmn001M06.do?requestedFilePath=rsvt";
+export const resource_cancel_url = "https://urs.kaist.ac.kr/urs/rsv/app/cmn/RsvAppCmn002M01_RU.do";
