02fbd130366ebe40b435b352a1875ab801b3ff6d
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2023-05-20 05:33:59 +0900

    feat: use cors

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { searchRouter } from "./router/search.router.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());         
 app.use(cookieParser());
 app.use("/api/search", searchRouter);
 app.use("/api/reservation", reservationRouter);
